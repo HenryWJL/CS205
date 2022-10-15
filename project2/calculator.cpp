3df5d5117dfd62dec841fe155498da55c7955f84
JunlinWang <114377159+HenryWJL@users.noreply.github.com>
2022-10-15 14:35:54 +0800

    Update calculator.cpp

--- project2/calculator.cpp
+++ project2/calculator.cpp
@@ -8,16 +8,60 @@ int fmax(int a,int b){
     return a>b?a:b;
 }
 
+bool isNotSmaller(string str1,string str2,int decimal1,int decimal2){      //判断两个正数数字字符串中的前是否大于等于后
+    int len1=str1.size();
+    int len2=str2.size();
+    int num1=len1-decimal1;
+    int num2=len2-decimal2;
+    if(num1==num2){
+        for(int i=0;i<num1;i++){
+            if(str1[i]-'0'>str2[i]-'0'){
+                return true;
+                break;
+            }
+            else if(str1[i]-'0'<str2[i]-'0'){
+                return false;
+                break;
+            }
+        }
+        string s1=str1.substr(num1);
+        string s2=str2.substr(num2);
+        if(s1.size()==s2.size()){
+            for(int i=0;i<s1.size();i++){
+                if(s1[i]-'0'>s2[i]-'0'){
+                    return true;
+                    break;
+                }
+                else if(s1[i]-'0'<s2[i]-'0'){
+                    return false;
+                    break;
+                }
+            }
+            return true;
+        }
+        else{
+            return s1.size()>s2.size()?true:false;
+        }
+    }
+    else{
+        return num1>num2?true:false;
+    }
+}
 
 string handler3(string* str){
     string str1=str[0];
     int decimal=atoi(str[1].c_str());
-    string str2="";
+    string str2,result;
     int power=0;
     stringstream ss;
+    bool isNegative=false;
+    if(str1[0]=='_'){
+        str1=str1.substr(1);
+        isNegative=true;
+    }
     if(decimal==0){
         if(str1.size()<=10){
-            return str1;
+            result=str1;
         }
         else{
             power=str1.size()-1;
@@ -28,11 +72,11 @@ string handler3(string* str){
             }
             str1=str1.substr(0,i+1);
             if(str1.size()==1){
-                return str1+".0"+"e"+ss.str();
+                result=str1+".0"+"e"+ss.str(); 
             }
             else{
                 str2=str1.insert(1,".");
-                return str2+"e"+ss.str();
+                result= str2+"e"+ss.str();
             }
         }
     }
@@ -48,11 +92,11 @@ string handler3(string* str){
                 
                 if(str1[i]=='.'){
                     str2=str2.substr(0,i);
-                    return str2;
+                    result= str2;
                 }
                 else{
                     str2=str2.substr(0,i+1);
-                    return str2;
+                    result= str2;
                 }
             }
             else{
@@ -66,14 +110,14 @@ string handler3(string* str){
                     i--;
                 }
                 str2=str2.substr(0,i+1);
-                return str2;
+                result= str2;
             }
         }
         else{
             power=str1.size()-decimal-1;
             ss<<power;
             if(str1.size()==1){
-                return str1+".0"+"e"+ss.str();
+                result= str1+".0"+"e"+ss.str();
             }
             else{
                 str2=str1.insert(1,".");
@@ -83,14 +127,15 @@ string handler3(string* str){
                 }
                 str2=str2.substr(0,i+1);
                 if(str2.size()==2){
-                    return str2+"0e"+ss.str();
+                    result= str2+"0e"+ss.str();
                 }
                 else{
-                    return str2+"e"+ss.str();
+                    result= str2+"e"+ss.str();
                 }
             }
         }
     }
+    return isNegative?"_"+result:result;
 }
 
 string* add(string num1,string num2,int decimal1,int decimal2){
@@ -156,6 +201,7 @@ string* add(string num1,string num2,int decimal1,int decimal2){
 string* subtract(string num1,string num2,int decimal1,int decimal2){
     stringstream ss;
     string result="";
+    string * result1=new string[2];
     //小数位对齐
     if(decimal1-decimal2<0){
         for(int i=0;i<decimal2-decimal1;i++){
@@ -167,16 +213,15 @@ string* subtract(string num1,string num2,int decimal1,int decimal2){
             num2=num2+'0';
         }
     }
-    
-    if(num1.size()>num2.size()){     //num1>num2
-        //整数位对齐
+    int decimal=fmax(decimal1,decimal2);
+    if(isNotSmaller(num1,num2,decimal,decimal)){
         int len1=num1.size();
         int len2=num2.size();
         for(int i=0;i<len1-len2;i++){
             num2='0'+num2;
         }
-        int outcome[num1.size()]={};
-        for(int i=num1.size()-1;i>=0;i--){
+        int outcome[len1]={};
+        for(int i=len1-1;i>=0;i--){
             int n1=num1[i]-'0';
             int n2=num2[i]-'0';
             if(n1<n2){
@@ -197,27 +242,33 @@ string* subtract(string num1,string num2,int decimal1,int decimal2){
             }
         }
         int i=0;
-        while(outcome[i]==0){
+        while(outcome[i]==0&&i<len1){   
             i++;
         }
-        for(;i<num1.size();i++){
-            result+=(outcome[i]+'0');
+        if(i==len1){
+            result1[0]="0";
+            result1[1]="0";
+            return result1;
         }
-        string * result1=new string[2];
-        result1[0]=result;
-        ss<<fmax(decimal1,decimal2);
-        result1[1]=ss.str();
-        return result1;
+        else{
+            for(;i<len1;i++){
+                result+=(outcome[i]+'0');
+            }
+            result1[0]=result;
+            ss<<decimal;
+            result1[1]=ss.str();
+            return result1;
+        }
     }
-    else if(num1.size()<num2.size()){     //num1<num2
+    else{
         //整数位对齐
         int len1=num1.size();
         int len2=num2.size();
         for(int i=0;i<len2-len1;i++){
             num1='0'+num1;
         }
-        int outcome[num1.size()]={};
-        for(int i=num1.size()-1;i>=0;i--){
+        int outcome[len2]={};
+        for(int i=len2-1;i>=0;i--){
             int n1=num1[i]-'0';
             int n2=num2[i]-'0';
             if(n2<n1){
@@ -241,104 +292,13 @@ string* subtract(string num1,string num2,int decimal1,int decimal2){
         while(outcome[i]==0){
             i++;
         }
-        for(;i<num1.size();i++){
+        for(;i<len2;i++){
             result+=(outcome[i]+'0');
         }
-        string * result1=new string[2];
         result1[0]="_"+result;
-        ss<<fmax(decimal1,decimal2);
+        ss<<decimal;
         result1[1]=ss.str();
         return result1;
-    }
-    else{
-        int a=0;
-        for(int i=0;i<num1.size();i++){
-            if((num1[i]-'0')>(num2[i]-'0')){
-                a=1;
-                break;
-            }
-            else if((num1[i]-'0')<(num2[i]-'0')){
-                a=2;
-                break;
-            }
-        }
-        if(a==0){     //num1=num2
-            string * result1=new string[2];
-            result1[0]="0";
-            result1[1]="0";
-            return result1;
-        }
-        else if(a==1){     //num1>num2
-            int outcome[num1.size()]={};
-            for(int i=num1.size()-1;i>=0;i--){
-                int n1=num1[i]-'0';
-                int n2=num2[i]-'0';
-                if(n1<n2){
-                    outcome[i]=n1+10-n2;
-                    i--;
-                    while((num1[i]-'0')<=(num2[i]-'0')){
-                        n1=num1[i]-'0';
-                        n2=num2[i]-'0';
-                        outcome[i]=n1+9-n2;
-                        i--;
-                    }
-                    n1=num1[i]-'0';
-                    n2=num2[i]-'0';
-                    outcome[i]=n1-n2-1;
-                }
-                else{
-                    outcome[i]=n1-n2;
-                }
-            }
-            int i=0;
-            while(outcome[i]==0){
-                i++;
-            }
-            for(;i<num1.size();i++){
-                result+=(outcome[i]+'0');
-            }
-            string * result1=new string[2];
-            
-            result1[0]=result;
-            ss<<fmax(decimal1,decimal2);
-            result1[1]=ss.str();
-            return result1;
-        }
-        else{    //num1<num2
-            int outcome[num1.size()]={};
-            for(int i=num1.size()-1;i>=0;i--){
-                int n1=num1[i]-'0';
-                int n2=num2[i]-'0';
-                if(n2<n1){
-                    outcome[i]=n2+10-n1;
-                    i--;
-                    while((num2[i]-'0')<=(num1[i]-'0')){
-                        n1=num1[i]-'0';
-                        n2=num2[i]-'0';
-                        outcome[i]=n2+9-n1;
-                        i--;
-                    }
-                    n1=num1[i]-'0';
-                    n2=num2[i]-'0';
-                    outcome[i]=n2-n1-1;
-                }
-                else{
-                    outcome[i]=n2-n1;
-                }
-            }
-            int i=0;
-            while(outcome[i]==0){
-                i++;
-            }
-            for(;i<num1.size();i++){
-                result+=(outcome[i]+'0');
-            }
-            string * result1=new string[2];
-            result1[0]="_"+result;
-            ss<<fmax(decimal1,decimal2);
-            result1[1]=ss.str();
-            return result1;
-        }
     }
 }
 
@@ -407,43 +367,6 @@ string* multiply(string str1,string str2,int decimal1,int decimal2){
     }   
     return finalResult;
 }
-
-bool isNotSmaller(string str1,string str2,int decimal1,int decimal2){      //判断两个正数数字字符串中的前是否大于等于后
-    if(str1.size()-decimal1==str2.size()-decimal2){
-        for(int i=0;i<str1.size()-decimal1;i++){
-            if(str1[i]-'0'>str2[i]-'0'){
-                return true;
-                break;
-            }
-            else if(str1[i]-'0'<str2[i]-'0'){
-                return false;
-                break;
-            }
-        }
-        string s1=str1.substr(str1.size()-decimal1);
-        string s2=str2.substr(str2.size()-decimal2);
-        if(s1.size()==s2.size()){
-            for(int i=0;i<s1.size();i++){
-                if(s1[i]-'0'>s2[i]-'0'){
-                    return true;
-                    break;
-                }
-                else if(s1[i]-'0'<s2[i]-'0'){
-                    return false;
-                    break;
-                }
-            }
-            return true;
-        }
-        else{
-            return s1.size()>s2.size()?true:false;
-        }
-    }
-    else{
-        return str1.size()-decimal1>str2.size()-decimal2?true:false;
-    }
-}
-
 
 string* divide(string str1,string str2,int decimal1,int decimal2){
     int decimal=decimal1-decimal2;
@@ -539,42 +462,92 @@ string* min(string str1,string str2,int decimal1,int decimal2){
     return str;
 }
 
-string* pow(string str1,string power,int decimal1,int decimal2){
-    string* str0=new string[2];
+string pow(string str1,string power,int decimal1,int decimal2){
+    string result;
+    string * str0=new string[2];
     stringstream ss;
+    bool isNegative=false;  //输入的str1是否为负数
+    int bound,finalDeci;    //bound为power的绝对值，finalDeci为结果的小数位
+    string str;             //一个在计算中使用的字符串
+    int decimal=decimal1;   //一个在计算中使用的小数位
+    //判断输入是否为负
+    if(str1[0]=='_'){
+        str1=str1.substr(1);
+        isNegative=true;
+    }
+    str=str1;
+    //判断power的正负
     if(power[0]=='0'){
-        str0[0]="1";
-        str0[1]="0";
+        str="0";
+        finalDeci=0;
     }
     else if(power[0]=='_'){
-        int bound=atoi(power.substr(1).c_str());
-        string str=str1;
-        int decimal=decimal1;
+        bound=atoi(power.substr(1).c_str());
         for(int i=0;i<bound-1;i++){
             str=multiply(str,str1,decimal,decimal1)[0];
             decimal=atoi(multiply(str,str1,decimal,decimal1)[1].c_str());
         }
-        int deci=atoi(divide("1",str,0,decimal)[1].c_str());
+        finalDeci=atoi(divide("1",str,0,decimal)[1].c_str());
         str=divide("1",str,0,decimal)[0];
-        ss<<deci;
-        str0[0]=str;
-        str0[1]=ss.str();
     }
     else{
-        int bound=atoi(power.c_str());
-        string str=str1;
-        int decimal=decimal1;
+        bound=atoi(power.c_str());
         for(int i=0;i<bound-1;i++){
             str=multiply(str,str1,decimal,decimal1)[0];
             decimal=atoi(multiply(str,str1,decimal,decimal1)[1].c_str());
         }
-        ss<<decimal;
+        finalDeci=decimal;
+    }
+    if(isNegative&&bound%2!=0){
+        ss<<finalDeci;
         str0[0]=str;
         str0[1]=ss.str();
+        result="_"+handler3(str0);
     }
-    return str0;        
+    else{
+        ss<<finalDeci;
+        str0[0]=str;
+        str0[1]=ss.str();
+        result=handler3(str0);
+    }
+    return result;        
 }
 
+string* abs(string * str){
+    if(str[0][0]=='_'){
+        str[0]=str[0].substr(1);
+        return str;
+    }
+    else{
+        return str;
+    }
+}
+
+string * sqrt(string * str){
+    if(str[0].compare("0")==0){
+        return str;
+    }
+    else if(str[0].compare("1")==0&&str[1].compare("0")==0){
+        return str;
+    }
+    else{
+        string n=handler3(str);
+        string w=n+"/2-1";
+        string k=CPU(w);
+        string str0;
+        string str1=k+"*"+k+"-"+n;
+        string str2=abs(subCPU(CPU(str1)))[0];
+        int str3=atoi(abs(subCPU(CPU(str1)))[1].c_str());
+        while(isNotSmaller(str2,"1",str3,1)){
+            str0="("+k+"+"+n+"/"+k+")/2";
+            k=CPU(str0);
+            str1=k+"*"+k+"-"+n;
+            str2=abs(subCPU(CPU(str1)))[0];
+            str3=atoi(abs(subCPU(CPU(str1)))[1].c_str());
+        }
+        return subCPU(k);
+    }
+}
 
 string calculator(string * str1,string * str2,char type){   //type指加减乘除和数学方法中的一种
     string num1o=str1[0];
@@ -683,7 +656,13 @@ string calculator(string * str1,string * str2,char type){   //type指加减乘�
     else if(type=='n'){        //min{}
         return handler3(min(num1o,num2o,decimal1,decimal2));
     }
-    else {                     //pow{}
-        return handler3(pow(num1o,num2o,decimal1,decimal2));
+    else if(type=='w'){        //pow{}
+        return pow(num1o,num2o,decimal1,decimal2);
+    }
+    else if(type=='t'){        //sqrt{}
+        return handler3(sqrt(str1));
+    }
+    else{                      //abs{}
+        return handler3(abs(str1));
     }    
 }
