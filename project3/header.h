c4bc9db8d86397a1584f16a4f115394e2ea9ff20
JunlinWang <114377159+HenryWJL@users.noreply.github.com>
2022-10-26 22:59:53 +0800

    Update header.h

--- project3/header.h
+++ project3/header.h
@@ -10,11 +10,20 @@ int matrixIsValid(const char * matrixString); //检测输入矩阵的格式
 int valueIsValid(char c);               //检测输入矩阵的元素值
 char * substr(const char * const string,int pos,int len);
 struct Matrix createMatrix(const char * matrixString);
-// struct Matrix createMatrix(int row,int column);
+struct Matrix createZeros(int row,int column);
+struct Matrix createOnes(int row,int column);
+struct Matrix createRand(int row,int column,int range);
 void deleteMatrix(struct Matrix * mpointer);
 void displayMatrix(const struct Matrix matrix);
 void copyMatrix(struct Matrix * mpointer,const struct Matrix templateMatrix);
 struct Matrix addMatrix(struct Matrix matrix1,struct Matrix matrix2);
-float maxValue(struct Matrix matrix);
-float minValue(struct Matrix matrix);
+struct Matrix subtractMatrix(struct Matrix matrix1,struct Matrix matrix2);
+struct Matrix multiplyMatrix(struct Matrix matrix1,struct Matrix matrix2);
+struct Matrix addScalar(struct Matrix matrix,float scalar);
+struct Matrix subtractScalar(struct Matrix matrix,float scalar);
+struct Matrix multiplyScalar(struct Matrix matrix,float scalar);
+float max(struct Matrix matrix);
+float min(struct Matrix matrix);
+float mean(struct Matrix matrix);
+float sum(struct Matrix matrix);
 #endif
