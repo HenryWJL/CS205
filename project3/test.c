31cae5591e60be575002830d5da9faddb5d86c0e
JunlinWang <114377159+HenryWJL@users.noreply.github.com>
2022-10-27 17:16:13 +0800

    Update test.c

--- project3/test.c
+++ project3/test.c
@@ -3,21 +3,35 @@
 
 int main()
 {
-    float a[4]={1,2,3,4};
-    float b[4]={4,5,6,7};
-    struct Matrix matrix=createMatrix("[a,2,3,4]");
-    struct Matrix matrix1=createMatrix("[4,5,6,7]");
-    struct Matrix matrix2=addMatrix(matrix,matrix1);
-    // createMatrix(&matrix,1,3);
-    // createMatrix(&matrix1,1,4);
-    // copyMatrix(&matrix1,matrix);
+    struct Matrix matrix=createMatrix("[[1.2,-2.5,-3],[2.7,-4,6.5],[3.1,5.2,7.9]]");
+    struct Matrix matrix1=createZeros(2,3);
+    struct Matrix matrix2=createOnes(3,3);
+    struct Matrix matrix3=createRand(1,3,9);
     displayMatrix(matrix);
     displayMatrix(matrix1);
     displayMatrix(matrix2);
-    // printf("%f\n",matrix2.content[2]);
+    displayMatrix(matrix3);
+    deleteMatrix(&matrix2);
+    copyMatrix(&matrix1,matrix3);
+    displayMatrix(matrix1);
+    displayMatrix(matrix2);
+    struct Matrix matrix4=multiplyMatrix(matrix1,matrix);
+    reshapeMatrix(&matrix1,3,1);
+    displayMatrix(matrix4);
+    displayMatrix(matrix1);
 
-    // for(int i=0;i<3;i++){
-    //     printf("%f\n",matrix.content[i]);
-    // }
+    matrix=createMatrix("[[1.2,-2.5,-3],[2.7,-4,6.5],[3.1,5.2,7.9]]");
+    matrix1=createZeros(2,3);
+    matrix2=createOnes(3,3);
+    struct Matrix matrix5=subtractMatrix(matrix,matrix2);
+    struct Matrix matrix6=addScalar(matrix1,5.2);
+    struct Matrix matrix7=multiplyScalar(matrix6,6.7);
+    displayMatrix(matrix5);
+    displayMatrix(matrix6);
+    displayMatrix(matrix7);
+    printf("%.2f\n",max(matrix5));
+    printf("%.2f\n",min(matrix6));
+    printf("%.2f\n",mean(matrix7));
 }
+
 
